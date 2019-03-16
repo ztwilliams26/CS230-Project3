eab2b32137a25281f3736b5222caef8cb431429c
ztwilliams26 <47344157+ztwilliams26@users.noreply.github.com>
2019-03-16 10:20:49 -0400

    Updates to BST.c and BST.h via upload
    To Do
    -
    Still need to add formatting to the output (proper line spacing, frequency of each word)
    Fix the order of the BST (currently reversed, just need to change to if statements but nothing hard)
    Add tree balancing and bubble sort (by frequency) if time permits

--- BST.c
+++ BST.c
@@ -1,9 +1,27 @@
 #include "BST.h"
-#include "string.h"
 
-typedef BST Node;
+typedef struct BST Node;
+
+int main(int argc, char* argv[]){  //to test
+  Node* newTree(char*);
+  Node* addWord(Node*, char*);
+  void printTree(Node*);
+  Node* root;
+  
+  root = newTree("map");
+  addWord(root, "apple");
+  addWord(root, "orange");
+  addWord(root, "grape");
+  addWord(root, "grape");
+  addWord(root, "nothing");
+  addWord(root, ".-kl!");
+  
+  printTree(root);
+  return 0;
+}
 
 Node* newTree(char* word){
+  Node* addWord(Node*, char*);
   return addWord(NULL, word);
 }
 
@@ -18,7 +36,7 @@ Node* addWord(Node* root, char* word){
     Node* newNode = (Node*) malloc(sizeof(Node));
     newNode -> word = word;
     newNode -> frequency = 1;
-    nweNode -> left = NULL;
+    newNode -> left = NULL;
     newNode -> right = NULL;
     return newNode;
   }
@@ -40,17 +58,54 @@ Node* addWord(Node* root, char* word){
 
 // Prints the entire contents of the BST using the write() system call
 void printTree(Node* root){
+  void nodePrint(Node*);
 
+  if(root->left!=NULL){
+    printTree(root->left);
+  }
+  nodePrint(root);
+  if(root->right!=NULL){
+    printTree(root->right);
+  }
 }
-    
+
+//Safely prints the word from a tree node using the write() system call. 
+//FUTURE WORK ~~~~~~~~~ Include the frequency and proper formatting
+void nodePrint(Node* root){
+  char* outNewLine(char*);
+  if(root->word!=NULL){
+    char* output = outNewLine(root->word);
+    int written = write(1, output, sizeof(output));
+    free(output);
+    if(written<0){
+      //Throw error somehow... perror? Or maybe that's not allowed
+      exit(-1);
+    }
+  }    
+}
+
 //Assuming word ends with the end of string character, decides if the string uses alphabetic characters (true=1 or false=0)
 //Might have problems without the EOS char present in a word
 int isAlpha(char* word){
   for(int i=0; word[i]!='\0'; i++){
-    if( ((int) word[i] < 65) || (((int) word[i] > 90) && ((int) word[i] < 97))) || ((int) word[i] > 122)))
-      return 0;
+    if( ((int) word[i] < 65) || 
+        (((int) word[i] > 90) && ((int) word[i] < 97)) || 
+        ((int) word[i] > 122)
+      ){
+        return 0;
+      }
   }
   return 1;
+}
+
+//Adds the \n character to a string 
+//DONT FORGET to FREE the returned string AFTER USE
+//FUTURE WORK ~~~~~~~~~ Include the frequency and proper formatting
+char* outNewLine(char* input){
+  char* output = (char*) malloc(sizeof(input)+1);
+  strcpy(output, input);
+  strcat(output, "\n");
+  return output;
 }
 
 // NOT NECESSARY - future improvements, use bubble sort to sort the tree by frequency after all words are stored. 

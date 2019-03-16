2d2bdc494001fe59f516ed9b42a12c2832156e54
ztwilliams26 <47344157+ztwilliams26@users.noreply.github.com>
2019-03-15 20:36:19 -0400

    Create BST.c

---
+++ BST.c
@@ -0,0 +1,24 @@
+typedef BST Node;
+
+Nodee* newTree(char* word){
+
+}
+
+Node* newWord(Node* root, char* word){
+
+}
+
+Node* searchWord(Node* root, char* word){
+
+}
+
+void printTree(Node* root){
+//only use the write() 
+
+}
+
+// NOT NECESSARY - future improvements, use bubble sort to sort the tree by frequency after all words are stored. 
+// WILL SCREW UP TREE if used before all words added
+void sortByFrequency(Node* root){
+
+}

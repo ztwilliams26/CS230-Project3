39eeb4a27d2ac4f725140678624b1db830e98830
ztwilliams26 <47344157+ztwilliams26@users.noreply.github.com>
2019-03-15 23:34:05 -0400

    Update BST.c
    Basic tree structure completed.
    Needs printTree yet.
    Still has future improvements of the bubblesort to be made.

--- BST.c
+++ BST.c
@@ -1,20 +1,56 @@
+#include "BST.h"
+#include "string.h"
+
 typedef BST Node;
 
-Nodee* newTree(char* word){
-
+Node* newTree(char* word){
+  return addWord(NULL, word);
 }
 
-Node* newWord(Node* root, char* word){
-
+//Checks for invalid input then adds the word to the root's tree (either incrementing the freq for existing words or makeing a new node for new ones)
+//Returns Node* as it works recursively
+Node* addWord(Node* root, char* word){
+  int isAlpha(char*);
+  if(!isAlpha(word) || word==NULL){
+    return NULL;
+  }
+  if(root == NULL){
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    newNode -> word = word;
+    newNode -> frequency = 1;
+    nweNode -> left = NULL;
+    newNode -> right = NULL;
+    return newNode;
+  }
+  int wordCompare = strcmp((root->word), word);
+  if(wordCompare==0){
+    root -> frequency++;
+    return root;
+  }
+  else if(wordCompare < 0){
+    root -> left = addWord(root->left, word);
+    return root;
+  }
+  else{
+    root -> right = addWord(root->right, word);
+    return root;
+  }
+  
 }
 
-Node* searchWord(Node* root, char* word){
-
-}
-
+// Prints the entire contents of the BST using the write() system call
 void printTree(Node* root){
-//only use the write() 
 
+}
+    
+//Assuming word ends with the end of string character, decides if the string uses alphabetic characters (true=1 or false=0)
+//Might have problems without the EOS char present in a word
+int isAlpha(char* word){
+  for(int i=0; word[i]!='\0'; i++){
+    if( ((int) word[i] < 65) || (((int) word[i] > 90) && ((int) word[i] < 97))) || ((int) word[i] > 122)))
+      return 0;
+  }
+  return 1;
 }
 
 // NOT NECESSARY - future improvements, use bubble sort to sort the tree by frequency after all words are stored. 

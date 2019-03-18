a61f4747c6365fd26fcf80bb06c0845fe1b26385
ztwilliams26 <47344157+ztwilliams26@users.noreply.github.com>
2019-03-17 22:21:28 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,26 +3,26 @@
 
 //NEED TO ADD ERROR HANDLING FOR OPEN AND CLOSE
 int main(int argc, char* argv){
-	//parser prototype?	
+	int fileTokenizer(int fd);	
 
 	//parse environmental variable if it exists
 	char* wordFreakEnv = getenv("WORD_FREAK");
 	if(wordFreakEnv != NULL){
 		int envFileDes = open(wordFreakEnv);		
-		parse(envFileDes);
+		fileTokenizer(envFileDes);
 		close(wordFreakEnv);
 	}	
 
 	//parse main arguments if they exist
 	for(int i=1; i<argc+1; i++){
 		int currentArgFile = open(argv[i]);
-		parse(currentArgFile);
+		fileTokenizer(currentArgFile);
 		close(currentArgFile;
 	}
 	
 	//parse stdin for any contents (might not have any)
 	//open(stdin) ~~~ already open as file descriptor 0	
-	parse(0)
+	fileTokenizer(0);
 	//close(stdin) ~~~ doesn't need to be closed
 	
 }
